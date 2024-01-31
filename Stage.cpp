eb4421a68eb83a8cb6d69071412aefec819408e4
琉聖 大友 <230641@jc-21.jp>
2024-01-31 16:40:41 +0900

    更新

--- Stage.cpp
+++ Stage.cpp
@@ -21,17 +21,39 @@ void Stage::Update()
 
 void Stage::Draw()
 {
-	for (int y = 0; y < 15; y++)
+	 const int STAGE_X = 15;
+	const int STAGE_Y = 15;
+	int STAGE_MAP[STAGE_Y][STAGE_X] = 
+	{ {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, 
+	  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1} };
+	
+	for (int y = 0; y < STAGE_Y; y++)
 	{
-		for (int x = 0; x < 15; x++)
+		for (int x = 0; x < STAGE_X; x++)
 		{
 			Transform floorTrans;
 			//Transform wallTrans;
 			floorTrans.position_ = { -6.5f+1.0f*(float)x,0,-5+1.0f*(float)y };
 			Model::SetTransform(hFloor_, floorTrans);
 			Model::SetTransform(hWall_, floorTrans);
-			Model::Draw(hFloor_);
-			if (x == 0 || x == 14 || y == 0 || y == 14)
+			if (STAGE_MAP[y][x] == 0)
+			{
+				Model::Draw(hFloor_);
+			}
+			else
 			{
 				Model::Draw(hWall_);
 			}
